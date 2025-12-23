ce5f27cbce8f1896887a06930f651df2e53563b0
cartao078 <documentosctc01@gmail.com>
2025-12-23 16:39:13 -0300

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -1,1 +1,16 @@
+// ================================
+// CONFIGURAÇÕES DE AMBIENTE
+// ================================
+
+// APIs INDEPENDENTES
+const ENV = {
+  CONTAGEM_APP: "https://script.google.com/macros/s/AKfycby6ip-dZkmDIrG3GhutwY3t40DprmNlUxIf9kMBefFwzosOFri9nq2dSArTQVb1CUxF/exec",
+  VENDAS: "https://script.google.com/macros/s/AKfycbz2iN8BFtjs0GHD1W84c0hNvRJIuJu3pmv-L0YcboGSgLhd2LQNOJMieJLTNWQC/exec",
+  REFUTURIZA: "https://script.google.com/macros/s/AKfycbwjrD4H1nK8CTpSVT0n7T9BYonh-F5s7zx3E9QVeiJuO1vgFGMCqnWgYH0einPPjxYl/exec",
+  RETENCAO: "https://script.google.com/macros/s/AKfycbzjQFDwj46u6Ymz33aT0SqVctD5Kvg4wkexeubg49rKl6OOXBoBdFnEASlFS5XRKhhZ/exec",
+  ADIMPLENCIA: "https://script.google.com/macros/s/AKfycbzJ5TZAmTBpoZiT4Wss9RvdguIPaPHaJrakHvqz0qReCyC21oweNt84feMFMvmC118U/exec",
+  CASHBACK: "https://script.google.com/macros/s/AKfycbxfI9vHstylYG7vjjf_5xDa_ElnBe9qcd6neiK4-bvoA6YrSK0MzY2Eq9jrAvOt2BnY/exec",
+
+  DEBUG: false
+};
 
